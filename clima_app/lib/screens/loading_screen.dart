93800815294e8b4a0255c5bf40b60718de546d36
unknown <richardcastuera16@gmail.com>
2023-06-30 11:59:04 +0800

    feat(clima_app): Added Spinner Package and Route

--- clima_app/lib/screens/loading_screen.dart
+++ clima_app/lib/screens/loading_screen.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:clima_app/services/location.dart';
 import 'package:clima_app/services/networking.dart';
+import 'package:clima_app/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = '17d1c91fa7072e277d12aa313eaf2e8d';
 
@@ -32,10 +34,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
     var weatherData = await networkHelper.getData();
     print(weatherData);
+
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context) {
+        return LocationScreen();
+      },
+    ));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 70.0,
+        ),
+      ),
+    );
   }
 }
